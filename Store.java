f45de469d82547fad70759d7278393f63794d0f6
agraw <agr.anish234@gmail.com>
2023-04-29 23:37:42 -0400

    Client Server for Stores

--- Store.java
+++ Store.java
@@ -1,6 +1,9 @@
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.net.*;
+import java.io.*;
+import javax.swing.*;
 
 /**
  * This sets up the store object used in other classes
@@ -15,20 +18,29 @@ public class Store {
     private String nameStore;
     private int messagesRec;
     private int messagesSent;
+    private Socket socket = null;
+    private OutputStream outputStream = null;
+    private PrintWriter writer = null;
+    private InputStream inputStream = null;
+    private BufferedReader reader = null;
 
     // constructor
     public Store(User user, String nameStore) {
-
-        this.user = user;
-        this.nameStore = nameStore;
         try {
+            socket = new Socket("localhost", 1234);
+            outputStream = socket.getOutputStream();
+            writer = new PrintWriter(outputStream, true);
+            InputStream inputStream = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            this.user = user;
+            this.nameStore = nameStore;
             String fileName = "sellerData.txt";
-            FileOutputStream fos = new FileOutputStream(fileName, true);
-            PrintWriter pw = new PrintWriter(fos);
             String sellerData = nameStore + ", " + user.getUsername();
-            pw.println(sellerData);
-            pw.close();
-        } catch (FileNotFoundException e) {}
+            String send = "/write/"+fileName + " {-/} "+ sellerData;
+            writer.println(send);
+    } catch (IOException e) {
+        JOptionPane.showMessageDialog(null, "Could not establish connection to server.");
+    }
     }
 
     // setters and getters
