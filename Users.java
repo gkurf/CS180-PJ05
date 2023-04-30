d1bbeac1f88ffce8f81caf3ebe430a4f377ce992
agraw <agr.anish234@gmail.com>
2023-04-30 17:43:56 -0400

    n

--- Users.java
+++ Users.java
@@ -82,6 +82,7 @@ public class Users {
         } catch (NullPointerException e) {
         } catch (IOException e) {
         }
+        System.out.println("File Content: " + fileContent);
         String[] fileContentArray = fileContent.split("\n");
         String line = "";
         String[] splitLine = {""};
